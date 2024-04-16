f35cf08dfc21e62e8305be89a795ab0005eeb755
Maruf Siyam <marufsiyam9123@gmail.com>
2024-04-16 13:22:16 +0600

    about section completed and also responsiveness done

--- src/Components/About_info_card/About_info_card.jsx
+++ src/Components/About_info_card/About_info_card.jsx
@@ -9,7 +9,22 @@ import { FaGithub } from "react-icons/fa";
 
 const About_info_card = () => {
   return (
-<div></div>
+    <div className="w-full h-[320px] esm:h-[500px] bg-[#000000] p-4 esm:p-10 2xl:p-16 rounded-xl flex flex-col justify-between">
+        <About_info title={'Name'} info={'Md. Maruf Hossain Siyam'}/>
+        <About_info title={'Email'} info={'marufsiyam9123@gmail.com'}/>
+        <About_info title={'Phone'} info={'+880 1826096711'}/>
+        <About_info title={'Address'} info={'Keranigonj, Dhaka'}/>
+        <div className="flex items-center justify-between">
+            <p className="text-white font-urbanist text-lg esm:text-xl lg:text-2xl">Follow Me</p>
+
+            <div className="flex gap-2 lg:gap-4">
+                <div className="text-white w-6 esm:w-8 lg:w-10 h-6 esm:h-8 lg:h-10 bg-slate-600 flex items-center justify-center rounded-full hover:bg-orange-100 hover:text-stone-900 duration-300"><FaFacebookF /></div>
+                <div className="text-white w-6 esm:w-8 lg:w-10 h-6 esm:h-8 lg:h-10 bg-slate-600 flex items-center justify-center rounded-full hover:bg-orange-100 hover:text-stone-900 duration-300"><FaLinkedinIn /></div>
+                <div className="text-white w-6 esm:w-8 lg:w-10 h-6 esm:h-8 lg:h-10 bg-slate-600 flex items-center justify-center rounded-full hover:bg-orange-100 hover:text-stone-900 duration-300"><FaDiscord /></div>
+                <div className="text-white w-6 esm:w-8 lg:w-10 h-6 esm:h-8 lg:h-10 bg-slate-600 flex items-center justify-center rounded-full hover:bg-orange-100 hover:text-stone-900 duration-300"><FaGithub /></div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,5 @@
-// import About_info_card from "../Components/About_info_card/About_info_card"
-// import about_profile from '../assets/1692521537641.jpeg'
+import About_info_card from "../Components/About_info_card/About_info_card"
+import about_profile from '../assets/1692521537641.jpeg'
 
 
 const About = () => {
@@ -12,7 +12,14 @@ const About = () => {
                 <h2 className="text-white font-urbanist font-semibold text-xl sm:text-2xl lg:text-3xl">Passionate about turning imaginative designs into seamless and responsive user interfaces.</h2>
             </div>
             
-
+            <div className="flex flex-col xl:flex-row gap-8 xl:gap-4 w-full mt-12 xl:mt-16">
+                <div className="w-full xl:w-1/2">
+                    <About_info_card/>
+                </div>
+                <div className=" h-[320px] esm:h-[500px] w-full xl:w-1/2 rounded-xl  overflow-hidden">
+                    <img className="object-cover max-h-full w-full rounded-xl" src={about_profile} alt="" />
+                </div>
+            </div>
     </div>
   )
 }
