e7b41c0a9f2d60329630d2dad39a65eb65325461
Maruf Siyam <marufsiyam9123@gmail.com>
2024-04-17 22:26:30 +0600

    skill section done and responisved also

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -22,7 +22,13 @@ const Skills = () => {
         <h2 className="text-white font-urbanist font-semibold text-xl text-center lg:text-start sm:text-2xl lg:text-3xl mt-12 indent-16 sm:indent-20 md:indent-30 lg:indent-40 leading-10">Elevating User Experiences: A Frontend Developer's Portfolio Spotlight. A Fresh Perspective in Interactive Design and Collaborative Endeavors</h2>
 
         <div className="flex gap-4 esm:gap-8 p-8 flex-wrap justify-center">
-            
+            <Skillset icon={<FaHtml5 />} title={'HTML'} color={'#FC4F13'}/>
+            <Skillset icon={<IoLogoCss3 />} title={'CSS'} color={'#0899DD'}/>
+            <Skillset icon={<SiTailwindcss  />} title={'TAILWIND'} color={'#3EBFF8'}/>
+            <Skillset icon={<IoLogoJavascript />} title={'JAVASCRIPT'} color={'#FFDF2B'}/>
+            <Skillset icon={<IoLogoReact />} title={'REACT'} color={'#08D9FF'}/>
+            <Skillset icon={<FaGitAlt />} title={'GIT'} color={'#F05539'}/>
+            <Skillset icon={<IoLogoFirebase />} title={'FIREBASE'} color={'#FFCD35'}/>
         </div>
     </div>
   )
