af465f954fa5657bb58b11ced1bc5d701d243be0
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-11 02:27:46 +0900

    some changes in ui

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -7,7 +7,7 @@ const Project = ({image, title, liveLink, gitLink}) => {
 
   return (
     <div className="w-full h-[350px] sm:h-[400px] lg:h-[450px] overflow-hidden relative rounded-xl group">
-          <img className="relative top-0 left-0 w-full h-auto transition-transform duration-1000 ease-in-out transform lg:group-hover:-translate-y-[calc(100%-450px)] group-hover:-translate-y-[calc(100%-400px)]" src={image} alt="" />
+          <img className="relative top-0 left-0 w-full h-auto transition-transform duration-1000 ease-in-out transform lg:group-hover:-translate-y-[calc(100%-450px)] sm:group-hover:-translate-y-[calc(100%-400px)] group-hover:-translate-y-[calc(100%-350px)]" src={image} alt="" />
           <div className="absolute flex esm:flex-row flex-col w-full h-[450px] top-0 left-0 bg-orange-400 bg-opacity-60 opacity-0 transition-opacity duration-150 delay-1000 group-hover:flex items-center justify-center gap-4 group-hover:opacity-100">
             <button className=" hover:scale-[1.1] transition duration-300 bg-slate-50 hover:text-orange-500 text-md sm:text-lg font-semibold px-4 py-2 rounded-xl font-urbanist">
               <a href={liveLink} target="_blank">Live View</a>
