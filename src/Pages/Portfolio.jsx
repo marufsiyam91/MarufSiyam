afd50d251cde3b81dfd2f889ace60a1dee81a0c1
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-11 02:07:27 +0900

    portfolio section modification

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,5 +1,8 @@
 import Project from "../Components/Project"
 import taskly from '../assets/taskly.png'
+import fazzfood from '../assets/faazfood.png'
+import travic from '../assets/travic website.png'
+import SectionBtn from './../Components/SectionBtn';
 
 
 const Portfolio = () => {
@@ -7,10 +10,24 @@ const Portfolio = () => {
 
 
   return (
-    <div className="py-20">
-        <h2 className="text-4xl text-white text-center">PORTFOLIO</h2>
-        <div>
-            <Project title={'Taskly'} image={taskly} liveLink={'https://taskly-tasker.netlify.app/'}/>
+    <div className="py-20 max-w-screen-4xl mx-auto">
+      <div className="flex justify-center mb-12">
+        <SectionBtn>Portfolio</SectionBtn>
+      </div>
+        <div className="flex w-full gap-6">
+            <Project image={fazzfood} title={'Taskly'} liveLink={'https://faazfood.netlify.app/'} gitLink={'https://github.com/marufsiyam91/faazfood'}/>
+            <Project image={travic} title={'Taskly'} liveLink={'http://localhost:5173/'}/>
+        </div>
+        <div className="relative max-w-[70%] mx-auto mt-8">
+          <img className="w-full h-full rounded-xl" src={taskly} alt="" />
+          <div className="rounded-xl absolute flex w-full h-full top-0 left-0 bg-orange-400 bg-opacity-60 opacity-0 transition-opacity duration-150 delay-200 hover:flex items-center justify-center gap-4 hover:opacity-100">
+            <button className=" hover:scale-[1.1] transition duration-300 bg-slate-50 hover:text-orange-500 text-lg font-semibold px-4 py-2 rounded-xl font-urbanist">
+              <a href="https://taskly-tasker.netlify.app/" target="_blank">Live View</a>
+            </button>
+            <button className=" hover:scale-[1.1] transition duration-300 bg-slate-50 hover:text-orange-500 text-lg font-semibold px-4 py-2 rounded-xl font-urbanist">
+              <a href="https://github.com/marufsiyam91/Taskly" target="_blank">Client Code</a>
+            </button>
+          </div>
         </div>
     </div>
   )
