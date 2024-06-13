e86a2ad03db06ec4b8c5098dd3094d54666a994f
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-13 23:22:07 +0600

    changes in style

--- src/Components/Skillset.jsx
+++ src/Components/Skillset.jsx
@@ -2,7 +2,7 @@
 
 const Skillset = ({icon, title, color}) => {
   return (
-        <div className="flex flex-col items-center justify-between p-6 esm:p-8 w-full esm:w-[180px] h-[200px] esm:h-[270px] gap-6 border border-orange-100 rounded-[5.5rem] esm:rounded-[6rem] group/circle">
+        <div className="flex flex-col items-center justify-between p-6 esm:p-8 w-[180px] h-[220px] esm:h-[270px] gap-6 border border-orange-100 rounded-[5.5rem] esm:rounded-[6rem] group/circle">
             <div className="w-24 h-24 flex items-center justify-center text-[3rem] bg-slate-700 rounded-full group-hover/circle:-translate-y-2 transition" style={{color: color}}>{icon}</div>
             <p className="text-orange-200 font-primary text-lg font-medium">{title}</p>
         </div>
